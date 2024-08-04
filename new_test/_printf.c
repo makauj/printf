4d00b55ed7c9b66b430847ec5b10febe0bfb8ce7
makauj <makauwanyoike@gmail.com>
2024-08-05 00:34:47 +0300

    New tests

---
+++ new_test/_printf.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _printf - Produces output according to a format
+ * @format: The format string
+ *
+ * Return: The number of characters printed (excluding the null byte)
+ */
+
+int _printf(const char *format, ...)
+{
+}
---
+++ new_test/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <limit.h>
+#include <unistd.h>
+#define BUFFER_SIZE 1020
+
+int _printf(const char *format, ...);
+
+#endif /* MAIN_H */
